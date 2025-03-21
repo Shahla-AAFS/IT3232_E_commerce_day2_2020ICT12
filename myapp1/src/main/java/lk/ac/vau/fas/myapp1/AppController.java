c9c440628ab3acfa45a901745f7821fca67e5f4f
Shahla-AAFS <shahla21999@gmail.com>
2025-03-21 20:48:27 +0530

    created the AppController and begins work on it

--- myapp1/src/main/java/lk/ac/vau/fas/myapp1/AppController.java
+++ myapp1/src/main/java/lk/ac/vau/fas/myapp1/AppController.java
@@ -1,4 +1,22 @@
-@Rest
+package lk.ac.vau.fas.myapp1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/app")
 public class AppController {
-    
+    @GetMapping("/msg")
+    public String myMessage()
+    {
+        return "Hello SpringBoot";
+    }
+
+    @GetMapping("/name")
+    public String myName()
+    {
+        return "My name is SpringBoot";
+    }
 }
